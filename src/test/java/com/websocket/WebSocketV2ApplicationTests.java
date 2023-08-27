c5bc11413eaf51e8495262fc76561520733c48a7
SUJINJEONG012 <peekaboo32@naver.com>
2023-08-27 18:57:35 +0900

    스프링부트로 웹소켓 공부하기2

---
+++ src/test/java/com/websocket/WebSocketV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
