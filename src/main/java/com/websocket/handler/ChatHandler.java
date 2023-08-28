178c90ae61c537689dbfeecf33c55ede00647714
angela <peekaboo32@naver.com>
2023-08-28 15:16:39 +0900

    chat핸들러 클래스에 TextWebSocketHandler 상속받아서 메서드 설정

--- src/main/java/com/websocket/handler/ChatHandler.java
+++ src/main/java/com/websocket/handler/ChatHandler.java
@@ -1,21 +1,67 @@
 package com.websocket.handler;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.log4j.Log4j2;
 
 @Component
 @Log4j2
 public class ChatHandler extends TextWebSocketHandler {
-	
+
 	private static List<String> onlineList = new ArrayList<>();
 	private static List<WebSocketSession> sessionList = new ArrayList<>();
 	Map<String, WebSocketSession> userSession = new HashMap<>();
+	
+	ObjectMapper json = new ObjectMapper();
+	
+	//Message
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		//json test
+		Map<String, Object> dataMap = new HashMap<>();
+		
+		//master status
+		String masterStatus = null;
+		if(userSession.containsKey("master")) {
+			masterStatus = "online";
+		}else {
+			masterStatus = "offline";
+		}
+	
+		//sending time
+		LocalDateTime currentTime = LocalDateTime.now();
+		String time = currentTime.format(DateTimeFormatter.ofPattern("hh:mm a, E"));
+		
+		//message data
+		String senderId = (String) session.getAttributes().get("sessionId");
+		String payload = message.getPayload();
+		
+		log.info("payload >>>>> "+ payload);
+		
+		dataMap = jsonToMap(payload);
+		dataMap.put("senderId", senderId);
+		dataMap.put("time", time);
+		dataMap.put("masterStatus", masterStatus);
+		dataMap.put("onlineList", onlineList);
+		
+		String receiverId = (String) dataMap.get("receiverId");
+		
+		log.info("final dataMap >>>>>>>  " + dataMap);
+		
+		
+	}	
 }
