21ab38efac18c326a53ffbc916126c0d8efb4ffa
SUJINJEONG012 <peekaboo32@naver.com>
2023-08-28 23:13:06 +0900

    HomeController getMapping 설정

--- src/main/java/com/websocket/controller/HomeController.java
+++ src/main/java/com/websocket/controller/HomeController.java
@@ -1,5 +1,20 @@
 package com.websocket.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+	@GetMapping("/")
+	public String main(HttpServletRequest request) {
+		return "index";
+	}
+	
+	@GetMapping("/test")
+	public String test(HttpServletRequest request) {
+		return "cookie";
+	}
 }
